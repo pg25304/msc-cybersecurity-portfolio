d05e641ccfff9196aa0ce504963f2bfc6149100b
capPayman <jezabel.jack@gmail.com>
2025-11-10 10:27:18 +0000

    credential design patterns

---
+++ jupyter-notebook/my_notebook.ipynb/CreationalDesignPatterns-Unit3-OOP/AbstractFactoryPattern.py
@@ -0,0 +1,40 @@
+#Product abstracts
+from abc import ABC, abstractmethod
+class Chair(ABC):
+    @abstractmethod
+    def sit_on(self):
+        pass
+class Sofa(ABC):
+    @abstractmethod
+    def lie_on(self):
+        pass
+#Concrete products
+class ModernChair(Chair):
+    def sit_on(self):
+        return "Sit on a modern chair"
+
+class ModernSofa(Sofa):
+    def lie_on(self):
+        return "ley on a modern sofa"
+#Abstract factory
+class FurnitureFactory(ABC):
+    @abstractmethod
+    def create_chair(self):
+        pass
+    @abstractmethod
+    def create_sofa(self0):
+        pass
+#concrete  Factory
+class ModernFurnitureFactory(FurnitureFactory):
+    def create_chair(self):
+        return ModernChair()
+    def create_sofa(self0):
+        return ModernSofa()
+
+
+#Client code
+factory = ModernFurnitureFactory()
+chair = factory.create_chair()
+sofa = factory.create_sofa()
+print(chair.sit_on())
+print(sofa.lie_on())
---
+++ jupyter-notebook/my_notebook.ipynb/CreationalDesignPatterns-Unit3-OOP/FactoryMethodDesign.py
@@ -0,0 +1,29 @@
+"""An abstract class is like a template or blueprint for other classes.
+It defines what methods a subclass must implement, but doesn’t provide
+full implementations itself.ABC stands for Abstract Base Class.
+It’s part of Python’s built-in abc module, which lets you define abstract classes —
+classes that are meant to be  templates and cannot be instantiated (create object directly from a class) directly."""
+from abc import ABC, abstractmethod
+class Vehicle(ABC):
+    @abstractmethod
+    def drive(self):
+        pass
+"""Car and Bike both inherit from Vehicle and provide their own implementation of the abstract drive() method.
+Now they are concrete classes — you can create instances of them because they have implemented 
+everything required by the base class."""
+class Car(Vehicle):
+    def drive(self):
+        return "Driving a car"
+
+class Bike(Vehicle):
+    def drive(self):
+        return "Riding a bike"
+
+#client code/test
+car = Car()
+print(car.drive())
+bike = Bike()
+print(bike.drive())
+# v=Vehicle() Error: Can't instantiate abstract class
+
+
